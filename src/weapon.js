01f3e40a539226420f706da4d2945d82a2fd1b55
Christopher Breen <C.jbreen61@gmail.com>
2021-03-09 19:19:24 -0500

    built out weapon class

---
+++ src/weapon.js
@@ -0,0 +1,11 @@
+class weapon{
+    constructor(name, image, attachment_1, attachment_2, attachment_3, attachment_4, attachment_5){
+        this.name = name;
+        this.image = image;
+        this.attachment_1 = attachment_1;
+        this.attachment_2 = attachment_2;
+        this.attachment_3 = attachment_3;
+        this.attachment_4 = attachment_4;
+        this.attachment_5 = attachment_5;
+    }
+}
